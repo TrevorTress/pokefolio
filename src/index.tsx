5b8b2d2969e3121b43f6ff168b4a015258f4f99e
TrevorTress <trevor.tress@outlook.com>
2023-08-07 12:12:07 -0400

    refactor to react

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
+import { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import styled from 'styled-components';
 
 const GameLogic: React.FC<{ className?: string }> = ({ className }) => {
-	const canvas: HTMLCanvasElement | null = document.querySelector('canvas');
-	if (canvas) {
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		if (!canvas) return;
+
+		const c = canvas.getContext('2d');
+		if (!c) return;
+
 		canvas.width = 1024;
 		canvas.height = 576;
-	}
 
-	return <canvas className={className}></canvas>;
+		c.fillRect(0, 0, canvas.width, canvas.height);
+	}, []);
+
+	return <canvas ref={canvasRef} className={className}></canvas>;
 };
 
 const Game = styled(GameLogic)`
