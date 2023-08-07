aef1b90ad3a78c8f809e0c7ca1c2637e9de5f229
TrevorTress <trevor.tress@outlook.com>
2023-08-07 12:38:05 -0400

    img loads

--- src/index.tsx
+++ src/index.tsx
@@ -7,24 +7,31 @@ const GameLogic: React.FC<{ className?: string }> = ({ className }) => {
 	const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
 	useEffect(() => {
+		// reference html canvas
 		const canvas = canvasRef.current;
 		if (!canvas) return;
 
+		// reference canvas context
 		const c = canvas.getContext('2d');
 		if (!c) return;
 
+		// assign canvas properties
 		canvas.width = 1024;
 		canvas.height = 576;
-
+		c.fillStyle = 'lightblue';
 		c.fillRect(0, 0, canvas.width, canvas.height);
+
+		const img = new Image();
+		img.src = '/game-map-v1.png';
+		img.onload = () => {
+			c.drawImage(img, 0, 0);
+		};
 	}, []);
 
 	return <canvas ref={canvasRef} className={className}></canvas>;
 };
 
-const Game = styled(GameLogic)`
-	background: lightblue;
-`;
+const Game = styled(GameLogic)``;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
