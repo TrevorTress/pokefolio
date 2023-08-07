e0bfa4ecceb07328c48c738071f98283f24f4982
TrevorTress <trevor.tress@outlook.com>
2023-08-07 12:01:37 -0400

    initial-code

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const GameLogic: React.FC<{ className?: string }> = ({ className }) => {
+	const canvas: HTMLCanvasElement | null = document.querySelector('canvas');
+	if (canvas) {
+		canvas.width = 1024;
+		canvas.height = 576;
+	}
+
+	return <canvas className={className}></canvas>;
+};
+
+const Game = styled(GameLogic)`
+	background: lightblue;
+`;
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<BrowserRouter>
+		<Game />
+	</BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
